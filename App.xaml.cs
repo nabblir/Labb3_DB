0561137300d6334b27e02b1399fa4ebcd0f2b108
Kevin Spehling <kevinspehling@live.se>
2026-01-12 17:03:31 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Labb3_DB
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
